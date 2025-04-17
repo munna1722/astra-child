bf8fd18bcaa52336106675935b4aaeb9580f9a9b
Munna <ibnesinamunna@gmail.com>
2025-04-17 14:44:59 +0600

    events post type and functions php

---
+++ event-post.php
@@ -0,0 +1,24 @@
+<?php
+function create_event_post_type() {
+    register_post_type('event',
+        array(
+            'labels' => array(
+                'name' => __('Events'),
+                'singular_name' => __('Event')
+            ),
+            'public'          => true,
+            'has_archive'     => true,
+            'menu_position'   => 5,
+            'menu_icon'       => 'dashicons-calendar',
+            'show_ui'         => true,
+            'show_in_menu'    => true,
+            'supports'        => array('title', 'editor', 'thumbnail'),
+            'capability_type' => 'post',
+            'capabilities'    => array(
+                'create_posts' => 'do_not_allow', // prevent non-admin from creating
+            ),
+            'map_meta_cap' => true,
+        )
+    );
+}
+add_action('init', 'create_event_post_type');
--- functions.php
+++ functions.php
@@ -5,3 +5,15 @@ function astra_child_enqueue_style(){
     wp_enqueue_style('astra-child-style', get_stylesheet_uri(  ), array('astra-theme-css'));
 }
 
+
+
+include_once('event-post.php');
+
+
+
+
+
+
+
+?>
+
