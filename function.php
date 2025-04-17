599823937e6d2d08f8d841c7effb5636a5af70a1
Munna <ibnesinamunna@gmail.com>
2025-04-17 13:57:19 +0600

    child theme creat

---
+++ function.php
@@ -0,0 +1,7 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'astra_child_enqueue_style');
+function astra_child_enqueue_style(){
+    wp_enqueue_style('astra-child-style', get_stylesheet_uri(  ), array('astra-theme-css'));
+}
+
